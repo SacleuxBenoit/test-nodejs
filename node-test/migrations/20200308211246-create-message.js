1181d70f1e2f934282ba2991266907cb7ad65290
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-08 22:15:50 +0100

    add references models Users and key: id in the migration message.js

--- node-test/migrations/20200308211246-create-message.js
+++ node-test/migrations/20200308211246-create-message.js
@@ -10,7 +10,11 @@ module.exports = {
       },
       idUser: {
         allowNull: false,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'Users',
+          key:'id',
+        }
       },
       title: {
         allowNull: false,
