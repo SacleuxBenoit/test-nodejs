e5fe91a224243441622161b354ec67037ec3f8db
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-08 15:42:22 +0100

    add content in the part instantiate server and configures routes in app.js

--- node-test/app.js
+++ node-test/app.js
@@ -1,8 +1,14 @@
 // Imports
-const express = require('express')
+const express = require('express');
 
 // Instantiate Server
+const server = express();
 
 // Configures routes
+
+server.get('/',function(req, res){
+  res.setHeader('Content-Type', 'text/html')
+  res.status(200).send('<h1>TEST</h1>')
+});
 
 // Launch Server
