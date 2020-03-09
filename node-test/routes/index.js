7700a9e19704a8f287f15ce1481515b97f213419
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-09 15:12:34 +0100

    add users routes for login and register in index.js

--- node-test/routes/index.js
+++ node-test/routes/index.js
@@ -5,4 +5,8 @@ const usersControllers = require('../controllers/users');
 // Router
 exports.router = (function(){
     const router = express.Router();
-})
+
+// Users Routes
+router.route('/users/register/').post(usersControllers.register);
+router.route('/users/login/').post(usersControllers.login);
+});
