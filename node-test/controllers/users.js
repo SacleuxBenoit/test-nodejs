fa148e55c4f1b47d2bfed442bcf2d9c6e6111918
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-09 14:44:16 +0100

    add the part register: and login: in the controller users.js (blank content for now)

--- node-test/controllers/users.js
+++ node-test/controllers/users.js
@@ -3,3 +3,11 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 // Routes
+module.exports = {
+    register: function(req, res){
+
+    },
+    login: function(req, res){
+        
+    }
+}
