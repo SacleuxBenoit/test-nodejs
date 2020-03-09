4cb670ec7d5aced36940063c780ada395ce9b84e
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-09 15:10:54 +0100

    add const router = express.Router in index.js

--- node-test/routes/index.js
+++ node-test/routes/index.js
@@ -1,3 +1,8 @@
 // Imports
 const express = require('express');
 const usersControllers = require('../controllers/users');
+
+// Router
+exports.router = (function(){
+    const router = express.Router();
+})
