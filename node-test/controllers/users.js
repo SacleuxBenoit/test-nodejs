24cc689bf8d6562b67fb59f2d16ba3ba088c0860
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-10 15:26:24 +0100

    add params for email username and password for the register in the controller users

--- node-test/controllers/users.js
+++ node-test/controllers/users.js
@@ -5,7 +5,10 @@ const jwt = require('jsonwebtoken');
 // Routes
 module.exports = {
     register: function(req, res){
-
+        // Params
+        const email = req.body.email;
+        const username = req.body.username;
+        const password = req.body.password;
     },
     login: function(req, res){
         
