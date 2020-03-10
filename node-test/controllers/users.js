cb967153f28385af2c337167970ed07c95a13666
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-10 15:28:16 +0100

    add a verification for see if some params is null in the controller users.js

--- node-test/controllers/users.js
+++ node-test/controllers/users.js
@@ -9,7 +9,13 @@ module.exports = {
         const email = req.body.email;
         const username = req.body.username;
         const password = req.body.password;
+
+        if (email == null || username == null|| password ==nul){
+            return res.status(400).json({'error':'missing parameters'});
+        }
     },
+
+
     login: function(req, res){
         
     }
