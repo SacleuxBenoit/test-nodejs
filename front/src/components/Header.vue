939b98a226598087caff408c8467f1b171a249c2
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-17 21:17:12 +0100

    add a template in the component Header.vue

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,11 +1,31 @@
 <template>
-    <div>
-        
-    </div>
+  <v-card class="overflow-hidden">
+    <div class="center">
+      <v-toolbar-title>Nom de l'entreprise</v-toolbar-title> 
+     <v-btn>
+      <router-link to="/"><strong>Redirection 1</strong></router-link>
+    </v-btn>
+    <v-btn>
+ <router-link to="/"><strong>Redirection 2</strong></router-link> 
+    </v-btn>
+    <v-btn>
+ <router-link to="/"><strong>Redirection 3</strong></router-link>
+    </v-btn>
+
+    <v-btn>
+ <router-link to="/"><strong>Redirection 4</strong></router-link> 
+    </v-btn>
+    <v-btn>
+ <router-link to="/"><strong>Redirection 5</strong></router-link>  
+   </v-btn>
+  </div>
+  </v-card>
 </template>
 
-<script>
-export default {
-    
+
+
+<style>
+.center{
+    text-align: center;
 }
-</script>
+</style>
