d43fabf600ce6447c6c75746cdc0efe46bc6ee83
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-17 21:12:43 +0100

    add some text in the component Footer

--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -1,10 +1,11 @@
+
 <template>
   <v-footer padless>
     <v-col
       class="text-center"
       cols="12"
     >
-      {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      {{ new Date().getFullYear() }} — <strong>Vuetify</strong> La partie Front est un test pour lier le back avec le front
     </v-col>
   </v-footer>
 </template>
