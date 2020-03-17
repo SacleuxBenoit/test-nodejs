d5f96c87f3afea3887bdafa782df465e0fcf0b79
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-17 21:10:13 +0100

    add a template of vuetify in the component Footer.vue

--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -1,11 +1,10 @@
 <template>
-    <div>
-        
-    </div>
+  <v-footer padless>
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+    </v-col>
+  </v-footer>
 </template>
-
-<script>
-export default {
-    
-}
-</script>
