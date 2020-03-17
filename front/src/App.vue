c5f8b18bc1804e15bcc301b4e255603b19bf94b8
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-17 21:11:57 +0100

    import the component Footer in App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="home">
     <router-view />
+    <Footer/>
   </div>
 </template>
 
 <script>
-
+import Footer from './components/Footer'
 
 export default {
   name: "App",
   components: {
-
+    Footer
   }
 };
 </script>
