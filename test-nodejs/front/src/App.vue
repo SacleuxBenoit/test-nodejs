999f4a20d0f94d018d4e7c8d7ab5a50a8b7e2ede
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-12 15:36:36 +0200

    add the folder front and back in test-nodejs

---
+++ test-nodejs/front/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <Header/>
+    <router-view />
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+export default {
+  name: "App",
+  components: {
+    Footer,
+    Header,
+  }
+};
+</script>
---
+++ test-nodejs/front/src/components/Footer.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <v-footer padless>
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      {{ new Date().getFullYear() }} — <strong>Vuetify</strong> La partie Front est un test pour lier le back avec le front
+    </v-col>
+  </v-footer>
+</template>
