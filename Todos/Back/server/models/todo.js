6de6960960ee712c4eacf2aa6b6c186defffe9a6
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-13 23:19:45 +0200

    refacto todo.js

--- Todos/Back/server/models/todo.js
+++ Todos/Back/server/models/todo.js
@@ -1,10 +1,17 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Todo = sequelize.define('Todo', {
-    title: DataTypes.STRING
-  }, {});
-  Todo.associate = function(models) {
-    // associations can be defined here
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  Todo.associate = (models) => {
+    Todo.hasMany(models.TodoItem, {
+      foreignKey: 'todoId',
+      as: 'todoItems',
+    });
   };
+
   return Todo;
 };
