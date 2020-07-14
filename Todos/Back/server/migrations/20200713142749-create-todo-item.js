276d525f944902e632a4d8fc431fe6107f51cb60
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-14 17:35:24 +0200

    add allowNull, defaultValue and todoId in create-todo-items.js

--- Todos/Back/server/migrations/20200713142749-create-todo-item.js
+++ Todos/Back/server/migrations/20200713142749-create-todo-item.js
@@ -1,30 +1,39 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('TodoItems', {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('TodoItems', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       content: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       complete: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('TodoItems');
-  }
+        type: Sequelize.DATE,
+      },
+      todoId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Todos',
+          key: 'id',
+          as: 'todoId',
+        },
+      },
+    }),
+  down: (queryInterface /* , Sequelize */) =>
+    queryInterface.dropTable('TodoItems'),
 };
