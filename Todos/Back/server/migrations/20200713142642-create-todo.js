923bed1c1a057f9f010ff9a487d37bb6c6725cfe
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-14 17:26:36 +0200

    refacto migrations/todo.js

--- Todos/Back/server/migrations/20200713142642-create-todo.js
+++ Todos/Back/server/migrations/20200713142642-create-todo.js
@@ -1,27 +1,26 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Todos', {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('Todos', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Todos');
-  }
+        type: Sequelize.DATE,
+      },
+    }),
+  down: (queryInterface /* , Sequelize */) => 
+    queryInterface.dropTable('Todos'),
 };
