bbc100d8d9aae4c6c0748e66cae81651d10afca1
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-15 15:15:48 +0200

    add a require server/routes in app.js

--- Todos/Back/app.js
+++ Todos/Back/app.js
@@ -2,15 +2,14 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
-const router = express()
-
+const router = express();
 router.use(logger('dev'));
-
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({extended : false}));
+router.use(bodyParser.urlencoded({ extended: false }));
 
-router.get('*', (req,res ) => res.status(200).send({
-    message : 'Welcome',
+require('./server/routes')(router);
+router.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome',
 }));
 
 module.exports = router;
