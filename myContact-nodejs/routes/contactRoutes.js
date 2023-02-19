7ee0a338cbc63154f45b3f0c829748e34b468566
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-02-19 16:58:15 +0100

    add route for post/put/delete

--- myContact-nodejs/routes/contactRoutes.js
+++ myContact-nodejs/routes/contactRoutes.js
@@ -5,4 +5,20 @@ router.route("/").get((req,res) => {
     res.status(200).json({message: "Get all contacts"});
 })
 
+router.route("/").post((req,res) => {
+    res.status(200).json({message: "Create contact"});
+})
+
+router.route("/:id").get((req,res) => {
+    res.status(200).json({message: `Get contact for ${req.params.id}`});
+})
+
+router.route("/:id").put((req,res) => {
+    res.status(200).json({message: `Update contact for ${req.params.id}`});
+})
+
+router.route("/:id").delete((req,res) => {
+    res.status(200).json({message: `delete contact for ${req.params.id}`});
+})
+
 module.exports = router;
