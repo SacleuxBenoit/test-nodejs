cf1f32b1e64a519f09f88c697f2e9d43900b5e5c
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-02-20 15:56:25 +0100

    get 1 contact depand on his id

--- myContact-nodejs/controllers/contactControllers.js
+++ myContact-nodejs/controllers/contactControllers.js
@@ -31,7 +31,12 @@ const createContact = asyncHandler(async (req,res) => {
 // route GET /api/contacts/:id
 // access public
 const getContact = asyncHandler(async (req,res) => {
-    res.status(200).json({message: `Get contact for ${req.params.id}`});
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    res.status(200).json({message: contact});
 })
 
 // @desc update contact
