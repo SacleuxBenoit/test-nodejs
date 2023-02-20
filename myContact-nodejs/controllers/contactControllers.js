96eb86e02c294f1d3d0d28451b9ec12b6c0f9e40
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-02-20 15:50:33 +0100

    find all contacts

--- myContact-nodejs/controllers/contactControllers.js
+++ myContact-nodejs/controllers/contactControllers.js
@@ -1,10 +1,11 @@
 const asyncHandler = require("express-async-handler");
-
+const Contact = require("../models/contactModel");
 // @desc Get all contacts
 // route GET /api/contacts
 // access public
 const getContacts = asyncHandler(async (req,res) => {
-    res.status(200).json({message: "Get all contacts"});
+    const contacts = await Contact.find();
+    res.status(200).json({contacts});
 })
 
 // @desc Create new contact
