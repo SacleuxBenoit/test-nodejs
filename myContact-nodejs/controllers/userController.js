4f8a86bc0f4ea3f9674a261f874da67e1196ae6f
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-02-21 16:33:10 +0100

    add jwt access token && update user login

--- myContact-nodejs/controllers/userController.js
+++ myContact-nodejs/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 // @desc Register a user
 // route POST /api/users/register
@@ -41,7 +42,28 @@ const registerUser = asyncHandler(async (req,res) => {
 // route POST /api/users/login
 // access public
 const userLogin = asyncHandler(async (req,res) => {
-    res.json({message: "login user"})
+    const { email, password } = req.body;
+    if(!email || !password){
+        res.status(400);
+        throw new Error("All field are mandatory");
+    }
+
+    const user = await User.findOne({email});
+
+    // compare password with hash password
+    if(user && (await bcrypt.compare(password, user.password))){
+        const accessToken = jwt.sign({
+            user:{
+                username: user.username,
+                email: user.email,
+                id: user.id
+            }
+        }, process.env.ACCESS_TOKEN_SECRET,{expiresIn: "1m"});
+        res.status(200).json({accessToken});
+    }else{
+        res.status(401);
+        throw new Error("email or password is not valid");
+    }
 })
 
 // @desc user current info
