485466d6272dbab605812f096c708e56d365f601
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-02-21 15:22:29 +0100

    user registration and hashed password

--- myContact-nodejs/controllers/userController.js
+++ myContact-nodejs/controllers/userController.js
@@ -1,9 +1,39 @@
 const asyncHandler = require("express-async-handler");
-
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 // @desc Register a user
 // route POST /api/users/register
 // access public
 const registerUser = asyncHandler(async (req,res) => {
+    const {username, email, password} = req.body;
+    if(!username || !email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory")
+    }
+
+    const userAvailable = await User.findOne({email});
+    if(userAvailable){
+        res.status(400);
+        throw new Error("User already register")
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("Hashed password", hashedPassword);
+
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword
+    })
+
+    console.log(`user created : ${user}`)
+    if(user){
+        res.status(201).json({_id: user.id, email: user.email});
+    }else{
+        res.status(400);
+        throw new Error("user data is not valid");
+    }
     res.json({message: "Register the user"});
 })
 
