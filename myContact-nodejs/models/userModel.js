9b0ca20e51a4a3b5b1f74705c3443c0b96c74075
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-02-21 15:06:25 +0100

    create model for user

---
+++ myContact-nodejs/models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        require: [true, "Please add the username"]
+    },
+    email: {
+        type: String,
+        require: [true, "Please add the email address"],
+        unique: [true, "Email address already taken"]
+    },
+    password: {
+        type: String,
+        require: [true, "Please add the user password"]
+    }
+},
+{
+    timestamps: true,
+})
+
+module.exports = mongoose.model("User", userSchema);
